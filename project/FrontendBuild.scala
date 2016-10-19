c2a56aa7bc532c13e7148caca17c6c97a829effb
somme <sommes.email@gmail.com>
2016-10-19 11:13:02 +0100

    increment versions

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -47,9 +47,9 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-config" % "2.0.1",
     "uk.gov.hmrc" %% "play-ui" % playUiVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "6.4.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "6.7.0",
     "uk.gov.hmrc" %% "play-json-logger" % "2.1.0",
-    "uk.gov.hmrc" %% "http-caching-client" % "5.3.0",
+    "uk.gov.hmrc" %% "http-caching-client" % "5.6.0",
     "joda-time" % "joda-time" % "2.8.2",
     "uk.gov.hmrc" %% "play-authorised-frontend" % "5.8.0"
   )
