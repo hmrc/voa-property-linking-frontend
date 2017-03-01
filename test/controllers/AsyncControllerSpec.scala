9189bf646beebc91e67a049da16f0f57efd8c14e
Mark Paxton <mark.paxton@digital.voa.gov.uk>
2017-03-01 14:10:45 +0000

    VTCCA-393 Return 400 and 404 for postcode lookups which are badly formated or result in no data

---
+++ test/controllers/AsyncControllerSpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import org.scalatest.{AppendedClues, AsyncFlatSpec, BeforeAndAfterEach, MustMatchers}
+import play.api.test.DefaultAwaitTimeout
+import utils._
+
+trait AsyncControllerSpec extends AsyncFlatSpec with MustMatchers with DefaultAwaitTimeout with BeforeAndAfterEach with AppendedClues {
+  implicit val app = TestApp.app
+
+  val token = "Csrf-Token" -> "nocheck"
+
+  override protected def beforeEach(): Unit = {
+    StubIndividualAccountConnector.reset()
+    StubGroupAccountConnector.reset()
+    StubAuthConnector.reset()
+    StubIdentityVerification.reset()
+    StubPropertyLinkConnector.reset()
+    StubAuthentication.reset()
+    StubBusinessRatesValuation.reset()
+  }
+}
+
