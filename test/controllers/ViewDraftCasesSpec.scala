0ea74d7916bb312a8e80b3ca63df83f06ef1aa29
richard.dennehy <richard.dennehy@digital.voa.gov.uk>
2017-07-18 11:37:51 +0100

    VTCCA-174 add effective date to draft cases

--- test/controllers/ViewDraftCasesSpec.scala
+++ test/controllers/ViewDraftCasesSpec.scala
@@ -17,15 +17,15 @@
 package controllers
 
 import connectors.{Authenticated, DraftCases}
-import models.{Accounts, DraftCase}
+import models.Accounts
 import org.jsoup.Jsoup
-import utils.{Formatters, StubAuthentication}
-import org.mockito.Mockito._
 import org.mockito.ArgumentMatchers._
+import org.mockito.Mockito._
 import play.api.test.FakeRequest
-import uk.gov.hmrc.play.http.HeaderCarrier
-import resources._
 import play.api.test.Helpers._
+import resources._
+import uk.gov.hmrc.play.http.HeaderCarrier
+import utils.{Formatters, StubAuthentication}
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Future
@@ -43,7 +43,7 @@ class ViewDraftCasesSpec extends ControllerSpec {
     html.select("p").asScala.map(_.text) must contain ("You have no saved draft cases")
   }
 
-  "Viewing draft cases, when the user has a saved draft" should "show the property address and draft expiry date" in {
+  "Viewing draft cases, when the user has a saved draft" should "show the property address, assessment effective date, and draft expiry date" in {
     val draftCase = randomDraftCase
     when(mockDraftCases.get(anyLong)(any[HeaderCarrier])).thenReturn(Future.successful(Seq(draftCase)))
     StubAuthentication.stubAuthenticationResult(Authenticated(Accounts(groupAccountGen, individualGen)))
@@ -54,9 +54,11 @@ class ViewDraftCasesSpec extends ControllerSpec {
     val html = Jsoup.parse(contentAsString(res))
     val rows = html.select("table tbody tr").asScala
     val address = rows.head.select("td").first.text
-    val expirationDate = rows.head.select("td").get(1).text
+    val effectiveDate = rows.head.select("td").get(1).text
+    val expirationDate = rows.head.select("td").get(2).text
 
     address mustBe Formatters.capitalizedAddress(draftCase.address)
+    effectiveDate mustBe Formatters.formatDate(draftCase.effectiveDate)
     expirationDate mustBe Formatters.formatDate(draftCase.expirationDate)
   }
 
@@ -69,7 +71,7 @@ class ViewDraftCasesSpec extends ControllerSpec {
     status(res) mustBe OK
 
     val html = Jsoup.parse(contentAsString(res))
-    val links = html.select("table tbody tr").asScala.head.select("td").get(2)
+    val links = html.select("table tbody tr").asScala.head.select("td").get(3)
     val resumeLink = links.select("li a").first.attr("href")
     val viewValuationLink = links.select("li a").get(1).attr("href")
 
@@ -94,11 +96,14 @@ class ViewDraftCasesSpec extends ControllerSpec {
       
       val address = columns.first.text
       address mustBe Formatters.capitalizedAddress(draft.address)
-        
-      val expirationDate = columns.get(1).text
+
+      val effectiveDate = columns.get(1).text
+      effectiveDate mustBe Formatters.formatDate(draft.effectiveDate)
+
+      val expirationDate = columns.get(2).text
       expirationDate mustBe Formatters.formatDate(draft.expirationDate)
-        
-      val links = columns.get(2).select("li a")
+
+      val links = columns.get(3).select("li a")
       links.first.attr("href") mustBe draft.url
       links.get(1).attr("href") mustBe routes.Assessments.viewDetailedAssessment(draft.propertyLinkId, draft.assessmentRef, draft.baRef).url
     }
