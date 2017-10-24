fede0bb49ea556b98437470baddfd3d3be01afce
daniel.connelly <daniel.connelly@digital.hmrc.gov.uk>
2017-10-24 11:48:11 +0100

    reverting back to inital values

--- app/controllers/Pagination.scala
+++ app/controllers/Pagination.scala
@@ -17,7 +17,7 @@
 package controllers
 
 import config.Global
-import play.api.mvc.{AnyContent, Request, Result}
+import play.api.mvc.{ AnyContent, Request, Result }
 import play.api.mvc.Results.BadRequest
 import play.twirl.api.Html
 import utils.Formatters._
@@ -27,8 +27,7 @@ import scala.collection.immutable.NumericRange.Inclusive
 import scala.concurrent.Future
 
 trait ValidPagination extends PropertyLinkingController {
-  protected def withValidPagination(page: Int, pageSize: Int, getTotal: Boolean = true)
-                                   (default: Pagination => Future[Result])(implicit request: Request[AnyContent]): Future[Result] = {
+  protected def withValidPagination(page: Int, pageSize: Int, getTotal: Boolean = true)(default: Pagination => Future[Result])(implicit request: Request[AnyContent]): Future[Result] = {
     if (page <= 0 || pageSize < 10 || pageSize > 100) {
       BadRequest(Global.badRequestTemplate)
     } else {
@@ -36,21 +35,23 @@ trait ValidPagination extends PropertyLinkingController {
     }
   }
 
-  protected def withValidPaginationSearchSort(page: Int,
-                                              pageSize: Int,
-                                              requestTotalRowCount: Boolean = true,
-                                              sortfield: Option[String] = None,
-                                              sortorder: Option[String] = None,
-                                              status: Option[String] = None,
-                                              address: Option[String] = None,
-                                              baref: Option[String] = None,
-                                              agent: Option[String] = None,
-                                              client: Option[String] = None)
-                                   (default: PaginationSearchSort => Future[Result])(implicit request: Request[AnyContent]): Future[Result] = {
+  protected def withValidPaginationSearchSort(
+    page: Int,
+    pageSize: Int,
+    requestTotalRowCount: Boolean = true,
+    sortfield: Option[String] = None,
+    sortorder: Option[String] = None,
+    status: Option[String] = None,
+    address: Option[String] = None,
+    baref: Option[String] = None,
+    agent: Option[String] = None,
+    client: Option[String] = None
+  )(default: PaginationSearchSort => Future[Result])(implicit request: Request[AnyContent]): Future[Result] = {
     if (page <= 0 || pageSize < 10 || pageSize > 100) {
       BadRequest(Global.badRequestTemplate)
     } else {
-      default(PaginationSearchSort(pageNumber = page,
+      default(PaginationSearchSort(
+        pageNumber = page,
         pageSize = pageSize,
         requestTotalRowCount = requestTotalRowCount,
         sortfield = sortfield,
@@ -59,7 +60,8 @@ trait ValidPagination extends PropertyLinkingController {
         address = address,
         baref = baref,
         agent = agent,
-        client = client))
+        client = client
+      ))
     }
   }
 }
@@ -110,13 +112,13 @@ case class PaginationSearchSort(
 
   def reverseSortOrder: Option[String] = {
 
-    sortorder match
-      { case Some(paramValue) if paramValue.toUpperCase == "ASC" => Some("DESC") ;
-        case _ => Some("ASC")
-      }
+    sortorder match {
+      case Some(paramValue) if paramValue.toUpperCase == "ASC" => Some("DESC");
+      case _ => Some("ASC")
+    }
   }
 
-  def valueOfSortorder : String = sortorder.getOrElse("ASC").toUpperCase
+  def valueOfSortorder: String = sortorder.getOrElse("ASC").toUpperCase
   def valueOfSortfield: String = sortfield.getOrElse("").trim
   def valueOfStatus: String = status.getOrElse("").trim
   def valueOfAddress: String = address.getOrElse("").trim
@@ -124,9 +126,8 @@ case class PaginationSearchSort(
   def valueOfAgent: String = agent.getOrElse("").trim
   def valueOfClient: String = client.getOrElse("").trim
 
-  def valuesOfSearchParameters : String =
+  def valuesOfSearchParameters: String =
     valueOfStatus + valueOfAddress + valueOfBaref + valueOfAgent + valueOfClient
-
 
   def startPoint: Int = pageSize * (pageNumber - 1) + 1
   override val toString = s"startPoint=$startPoint&pageSize=$pageSize&requestTotalRowCount=$requestTotalRowCount" +
@@ -143,30 +144,30 @@ case class PaginationSearchSort(
   def getHtml: List[Html] = {
     val list = (1 to maxPage).map(html).toList
     if (list.isEmpty)
-      List(Html("""<a class="next pX">Next</a>"""), Html("""<a class="pX value" id="current" data-dt-idx="1">1</a><a class="pX value" data-dt-idx="2">2</a><a class="pX value" data-dt-idx="3">3</a><a class="pX value" data-dt-idx="4">4</a><a class="pX value" data-dt-idx="5">5</a><a>...</><a class="pX value" data-dt-idx="8">8</a>"""), Html("""<a class="previous pX" >Previous</a>"""))
+      List(Html("""<a class="next pX disabled">Next</a>"""), Html("""<a class="value" id="current" data-dt-idx="1">1</a>"""), Html("""<a class="previous pX disabled" >Previous</a>"""))
     else
       previous ::: list ::: next
   }
 
   private def previous: List[Html] = {
     if (pageNumber == 1)
-      List(Html("""<a class="pagination disabled">Previous</a>"""))
+      List(Html("""<a class="pX disabled">Previous</a>"""))
     else
-      List(Html(s"""<a class="pagination" data-dt-idx="${pageNumber - 1}" >Previous</a>"""))
+      List(Html(s"""<a class="pX" data-dt-idx="${pageNumber - 1}" >Previous</a>"""))
   }
 
   private def next: List[Html] = {
     if (pageNumber == maxPage)
-      List(Html("""<a class="pagination" >Next</a>"""))
+      List(Html("""<a class="pX disabled" >Next</a>"""))
     else
-      List(Html(s"""<a class="pagination" data-dt-idx="${pageNumber + 1}" >Next<a>"""))
+      List(Html(s"""<a class="pX" data-dt-idx="${pageNumber + 1}" >Next<a>"""))
   }
 
   private def html(int: Int): Html = {
     if (int == pageNumber)
-      Html(s"""<a class="pagination" id="current" data-dt-idx="$int" >$int</a>""")
+      Html(s"""<a class="pX value" id="current" data-dt-idx="$int" >$int</a>""")
     else
-      Html(s"""<a class="pagination" data-dt-idx="$int" >$int</a>""")
+      Html(s"""<a class="pX value" data-dt-idx="$int" >$int</a>""")
   }
 }
 
