b42426486d4525c68f9c88d17e4526c5db6835f1
Darrell Plant <darrell.plant@gmail.com>
2017-10-25 15:13:05 +0100

    VTCCA-1032 disinstructed agents (#302)
    VTCCA-1032 filter out disinstructed agents

--- app/connectors/propertyLinking/PropertyLinkConnector.scala
+++ app/connectors/propertyLinking/PropertyLinkConnector.scala
@@ -22,7 +22,7 @@ import javax.inject.Inject
 import connectors.fileUpload.FileMetadata
 import controllers.{Pagination, PaginationSearchSort}
 import models._
-import models.searchApi.OwnerAuthResult
+import models.searchApi.{OwnerAuthAgent, OwnerAuthResult}
 import session.LinkingSessionRequest
 import uk.gov.hmrc.play.config.inject.ServicesConfig
 import uk.gov.hmrc.play.http._
@@ -66,17 +66,30 @@ class PropertyLinkConnector @Inject()(config: ServicesConfig, http: WSHttp)(impl
 
   def linkedPropertiesSearchAndSort(organisationId: Long,
                                     pagination: PaginationSearchSort)
-                      (implicit hc: HeaderCarrier): Future[OwnerAuthResult] = {
-    http.GET[OwnerAuthResult](s"$baseUrl/property-links-search-sort?" +
+                                   (implicit hc: HeaderCarrier): Future[OwnerAuthResult] = {
+
+    val ownerAuthResult = http.GET[OwnerAuthResult](s"$baseUrl/property-links-search-sort?" +
       s"organisationId=$organisationId&" +
       s"$pagination")
+
+    def validAgent(agent: OwnerAuthAgent) = {
+      Seq(RepresentationApproved.name, RepresentationPending.name)
+        .contains(agent.status.getOrElse(RepresentationApproved.name).toUpperCase)
+    }
+
+    // filter out agents with status other than Approved, Pending
+    // if status is not present, then keep the agent
+    ownerAuthResult.map(oar =>
+      oar.copy(authorisations = oar.authorisations.map(auth =>
+        auth.copy(agents = auth.agents.map(ags => ags.filter(ag => validAgent(ag)))))))
+
   }
 
   def clientProperty(authorisationId: Long, clientOrgId: Long, agentOrgId: Long)(implicit hc: HeaderCarrier): Future[Option[ClientProperty]] = {
     val url =
       s"$baseUrl/property-links/client-property/$authorisationId" +
-         s"?clientOrganisationId=$clientOrgId" +
-         s"&agentOrganisationId=$agentOrgId"
+        s"?clientOrganisationId=$clientOrgId" +
+        s"&agentOrganisationId=$agentOrgId"
 
     http.GET[Option[ClientProperty]](url) recover { case _: NotFoundException => None }
   }
