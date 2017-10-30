48933b1da8e429ca0e7eeeeb7460c4b71e8cd84e
Richard Dennehy <richard.dennehy@digital.voa.gov.uk>
2017-10-30 16:01:47 +0000

    VTCCA-963 add js enhancements to view messages

---
+++ app/assets/javascripts/src/viewMessage.js
@@ -0,0 +1,97 @@
+(function () {
+
+    'use strict';
+
+    var root = this, $ = root.jQuery;
+
+    if (typeof VOA === 'undefined') {
+        root.VOA = {};
+    }
+
+    root.VOA.viewMessage = function () {
+        $('.message-link').click(function (e) {
+            e.preventDefault();
+
+            markMessageAsRead($(this).closest('tr'));
+
+            var viewMessageUrl = $(this).context.href;
+            $.get(viewMessageUrl).then(function (viewMessagePage) {
+                var modal = createModal(viewMessagePage, viewMessageUrl);
+
+                $(document).find('.content_body').after(modal);
+
+                $('#print-message').click(function() {
+                    $('.modal-content-div').printThis();
+                });
+            });
+        });
+
+        $('.message--row').click(function (e) {
+            markMessageAsRead($(this));
+
+            var viewMessageUrl = $(this).find('a').first().attr('href');
+            $.get(viewMessageUrl).then(function (viewMessagePage) {
+                var modal = createModal(viewMessagePage, viewMessageUrl);
+
+                $(document).find('.content_body').after(modal);
+
+                $('#print-message').click(function() {
+                    $('.modal-content-div').printThis();
+                });
+            });
+        });
+
+        function markMessageAsRead(row) {
+            if(row.hasClass('message--row__unread')) {
+                row.removeClass('message--row__unread').addClass('message--row__read');
+
+                var readStatus = row.find('i');
+                readStatus.removeClass('icon-messageunread').addClass('icon-messageread');
+                readStatus.find('span').text('Read');
+
+                var messageTab = $('ul[role="tablist"] li.active a');
+                messageTab.text(decrementMessageCount);
+
+                var unreadCount = $('#unread-message-count').find('a');
+                unreadCount.text(decrementMessageCount);
+            }
+        }
+
+        function decrementMessageCount(idx, text) {
+            return text.replace(/(\d+)/, function(d) {
+                return d !== 0 ? d - 1 : d;
+            });
+        }
+
+        function createModal(message, viewMessageUrl) {
+            var content = $(message).find('#messageContent');
+
+            var pdfLink = '<p><a id="message-pdf" href="' + viewMessageUrl + '/pdf' + '" target="_blank">Download this message as a PDF</a></p>';
+
+            var printLink = '<p><a id="print-message" href="#">Print this message</a></p>';
+
+            var closeIcon =
+                '<button class="dialog-close" aria-label="close" type="button">' +
+                    '<span class="icon icon-close">x</span>' +
+                '</button>';
+
+            var closeButton = '<a href="#" class="margin-top-10 dialog-cancel button-secondary" aria-label="close" role="button">Close</a>';
+
+            return '' +
+                '<div id="dialog-message" class="dialog dialog-scroll" aria-hidden="false">' +
+                    '<div class="dialog-holder">' +
+                        '<div id="dialog-content-message" class="dialog-content">' +
+                            closeIcon +
+                            '<div class="modal-content-div">' +
+                                content.html() +
+                            '</div>' +
+                            pdfLink +
+                            printLink +
+                            closeButton +
+                        '</div>' +
+                    '</div>' +
+                '</div>';
+        }
+    };
+
+}).call(this);
