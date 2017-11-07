f94ecb6c2b3d3fb400ba753851220b9d1c46ad57
fi <feeon@live.com>
2017-11-07 16:10:24 +0000

    initial work on accepting multiple agent requests

--- app/assets/javascripts/src/dataTablePendingRequests.js
+++ app/assets/javascripts/src/dataTablePendingRequests.js
@@ -32,6 +32,7 @@
                 }
             },
             columns: [
+                {data: null, defaultContent: 'TICK'},
                 {data: 'organisationName'},
                 {data: 'address'},
                 {data: null, defaultContent: '<ul class="list"><li></li><li></li></ul>'},
@@ -39,14 +40,27 @@
                 {data: null, defaultContent: '<ul class="list"><li></li><li></li></ul>'}
             ],
             fnRowCallback: function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-                $('td:eq(2) ul li:eq(0)', nRow).html( messages.labels.check + ': ' + messages.labels['status' + aData.checkPermission]);
-                $('td:eq(2) ul li:eq(1)', nRow).html( messages.labels.challenge + ': ' + messages.labels['status' + aData.challengePermission]);
-                $('td:eq(3)', nRow).text(moment(aData.createDatetime).format('LL'));
-                $('td:eq(4) ul li:eq(0)', nRow).html('<a href="/business-rates-property-linking/representation-request/accept/' + aData.submissionId + '/' + results +'">' + messages.labels.accept + '</a>');
-                $('td:eq(4) ul li:eq(1)', nRow).html('<a href="/business-rates-property-linking/representation-request/reject/' + aData.submissionId + '/' + results +'">' + messages.labels.reject + '</a>');
+                $('td:eq(3) ul li:eq(0)', nRow).html( messages.labels.check + ': ' + messages.labels['status' + aData.checkPermission]);
+                $('td:eq(3) ul li:eq(1)', nRow).html( messages.labels.challenge + ': ' + messages.labels['status' + aData.challengePermission]);
+                $('td:eq(4)', nRow).text(moment(aData.createDatetime).format('LL'));
+                $('td:eq(5) ul li:eq(0)', nRow).html('<a href="/business-rates-property-linking/representation-request/accept/' + aData.submissionId + '/' + results +'">' + messages.labels.accept + '</a>');
+                $('td:eq(5) ul li:eq(1)', nRow).html('<a href="/business-rates-property-linking/representation-request/reject/' + aData.submissionId + '/' + results +'">' + messages.labels.reject + '</a>');
             }
         });
 
     };
 
+    $('#select-all').click(function(event) {
+        var allselected = true;
+        $('input[type="checkbox"]').each(function () {
+            allselected = allselected && $(this).is(':checked');
+        });
+
+        $('input[type="checkbox"]').each(function () {
+            $(this).prop('checked', !allselected);
+        });
+        return false;
+    });
+
 }).call(this);
+
