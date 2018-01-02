c36ac7db4bfbac9a74d2feeb97625fbe391e23b9
Darrell Plant <darrell.plant@gmail.com>
2018-01-02 15:19:40 +0000

    VTCCA-1316 add check summary link to messages

--- app/config/ApplicationConfig.scala
+++ app/config/ApplicationConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ import uk.gov.hmrc.play.config.inject.RunMode
 class ApplicationConfig @Inject()(configuration: Configuration, runMode: RunMode) {
   def baseUrl: String = if (runMode.env == "Prod") "" else "http://localhost:9523"
   def businessRatesValuationUrl(page: String): String = getConfig("business-rates-valuation.url") + s"/$page"
+  def businessRatesCheckUrl(page: String): String = getConfig("business-rates-check.url") + s"/$page"
 
   val helpGuideUrl = getConfig("help-guide.url")
 
