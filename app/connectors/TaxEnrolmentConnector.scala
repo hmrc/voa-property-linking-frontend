a466b35e9f8c4c0ae5e26871a26501769a53820b
daniel.connelly <daniel.connelly@digital.hmrc.gov.uk>
2018-01-09 16:36:21 +0000

    some weird error

--- app/connectors/TaxEnrolmentConnector.scala
+++ app/connectors/TaxEnrolmentConnector.scala
@@ -39,6 +39,7 @@ class TaxEnrolmentConnector @Inject()(wSHttp: WSHttp) extends ServicesConfig {
   def deEnrol(implicit hc: HeaderCarrier, ex: ExecutionContext) =
     wSHttp.POSTEmpty[HttpResponse](s"$serviceUrl/tax-enrolments/de-enrol/HMRC-VOA-CCA")
 
+
   private def enrolMaybe(enrolmentPayload: EnrolmentPayload)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
     wSHttp.PUT[EnrolmentPayload, HttpResponse](s"$serviceUrl/tax-enrolments/service/HMRC-VOA-CCA/enrolment", enrolmentPayload)
 
