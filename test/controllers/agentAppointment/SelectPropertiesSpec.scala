3aa1c2f547d17718db99e86bfe1ef4315913703a
fi <feeon@live.com>
2018-02-19 08:22:06 +0000

    VTCCA-1028 writing tests

--- test/controllers/agentAppointment/SelectPropertiesSpec.scala
+++ test/controllers/agentAppointment/SelectPropertiesSpec.scala
@@ -16,8 +16,6 @@
 
 package controllers.agentAppointment
 
-import config.ApplicationConfig
-import connectors.propertyLinking.PropertyLinkConnector
 import connectors.{AgentsConnector, Authenticated, GroupAccounts, _}
 import controllers.ControllerSpec
 import models.searchApi._
@@ -68,11 +66,90 @@ class SelectPropertiesSpec extends ControllerSpec {
 //      case _ => "None"
 //    }
 //
-//    checkTableColumn(html, 3, "Agent", agents)
+//    checkTableColumn(html, 3, "Appointed Agents", agents)
 //  }
 
 
 
+  it must "display the current page number" in {
+    val html = defaultHtml
+
+    html.select("ul.pagination li.active").text mustBe "1"
+  }
+
+  it must "include a 'next' link if there are more results" in {
+    setup(numberOfLinks = 16)
+
+    val res = TestAppointAgentController.selectProperties(AgentPropertiesPagination())(FakeRequest())
+    status(res) mustBe OK
+
+    val html = Jsoup.parse(contentAsString(res))
+
+    val expectedPagination = AgentPropertiesPagination(pageNumber = 2)
+
+    val nextLink = html.select("ul.pagination li.next")
+
+    nextLink.hasClass("disabled") mustBe false withClue "'Next' link is incorrectly disabled"
+    nextLink.select("a").attr("href") mustBe routes.AppointAgentController.selectProperties(expectedPagination).url
+  }
+//Need to fix in view file
+//  it must "include an inactive 'next' link if there are no further results" in {
+//    setup(numberOfLinks = 16)
+//
+//    val res = TestAppointAgentController.selectProperties(AgentPropertiesPagination())(FakeRequest())
+//    status(res) mustBe OK
+//
+//    val html = Jsoup.parse(contentAsString(res))
+//
+//    val nextLink = html.select("ul.pagination li.next")
+//
+//    nextLink.hasClass("disabled") mustBe true withClue "'Next' link is not disabled"
+//  }
+
+  //this shouldn't be passing
+//  it must "include an inactive 'previous' link when on page 1" in {
+//    setup(numberOfLinks = 16)
+//
+//    val res = TestAppointAgentController.selectProperties(AgentPropertiesPagination())(FakeRequest())
+//    status(res) mustBe OK
+//
+//    val html = Jsoup.parse(contentAsString(res))
+//
+//    val previousLink = html.select("ul.pagination li.previous")
+//
+//    previousLink.hasClass("disabled") mustBe true withClue "'Previous' link is not disabled"
+//  }
+//
+//  it must "include a 'previous' link when not on page 1" in {
+//    setup(numberOfLinks = 16)
+//
+//    val res = TestAppointAgentController.selectProperties(AgentPropertiesPagination())(FakeRequest())
+//    status(res) mustBe OK
+//
+//    val html = Jsoup.parse(contentAsString(res))
+//
+//    val previousLink = html.select("ul.pagination li.previous")
+//
+//    previousLink.hasClass("disabled") mustBe false withClue "'Previous' link is incorrectly disabled"
+//    previousLink.select("a").attr("href") mustBe routes.AppointAgentController.selectProperties().url
+//  }
+
+//  it must "include pagination controls" in {
+//    val html = defaultHtml
+//
+//    val pageSizeControls = html.select("ul.pageLength li").asScala
+//
+//    pageSizeControls must have size 4
+//    pageSizeControls.head.text mustBe "15"
+//
+//    val paginationControls = AgentPropertiesPagination(pageSize = 1.toString(n))
+//
+//    val paginationControls: Int => String = n => routes.AppointAgentController.selectProperties
+//
+//    val managePropertiesLink: Int => String = n => routes.Dashboard.manageProperties(pageSize = n).url
+//
+//    pageSizeControls.tail.map(_.select("a").attr("href")) must contain theSameElementsAs Seq(paginationControls(25), paginationControls(50), paginationControls(100))
+//  }
 
   private def setup(numberOfLinks: Int = 15) = {
     val groupAccount: GroupAccount = arbitrary[GroupAccount]
