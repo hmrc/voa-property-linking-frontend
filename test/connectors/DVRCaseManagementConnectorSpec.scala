eb012fea83df4b70c0574bec07b110fd91971e68
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2018-05-30 16:39:36 +0100

    VTCCA-1763 Some minor tweaks and tidy up:

--- test/connectors/DVRCaseManagementConnectorSpec.scala
+++ test/connectors/DVRCaseManagementConnectorSpec.scala
@@ -40,7 +40,6 @@ class DVRCaseManagementConnectorSpec extends VoaPropertyLinkingSpec {
   }
 
   "dvrExists" must "successfully return a boolean" in new Setup {
-
     mockHttpGET[Boolean]("tst-url", true)
     whenReady(connector.dvrExists(1,2))(_ mustBe true)
   }
