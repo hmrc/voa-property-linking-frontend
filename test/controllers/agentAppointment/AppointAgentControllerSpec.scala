1163b46d7bd77996f3cde0094389dccace778dfc
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2018-07-09 14:41:37 +0100

    Remove temp test

--- test/controllers/agentAppointment/AppointAgentControllerSpec.scala
+++ test/controllers/agentAppointment/AppointAgentControllerSpec.scala
@@ -146,23 +146,6 @@ class AppointAgentControllerSpec extends VoaPropertyLinkingSpec with MockitoSuga
     page.mustContainText(s"You have appointed agent '${testAgentAccount.companyName}' (${testAgentAccount.agentCode}) with the ability to submit checks and challenges to the selected properties.")
   }
 
-  "createAndSubmitAgentRepRequest" should "submit an agent representation request" in {
-    stubLogin()
-
-    when(mockPropertyLinkConnector.get(any(), any())(any[HeaderCarrier])).thenReturn(Future.successful(Some(testPropertyLink)))
-
-    val res = testController.createAn()(FakeRequest().withFormUrlEncodedBody(
-      "agentCode" -> "1",
-      "checkPermission" -> StartAndContinue.name,
-      "challengePermission" -> StartAndContinue.name,
-      "linkIds[]" -> "123"
-    ))
-
-    status(res) mustBe OK
-    val page = HtmlPage(Jsoup.parse(contentAsString(res)))
-    page.mustContainText(s"You have appointed agent '${testAgentAccount.companyName}' (${testAgentAccount.agentCode}) with the ability to submit checks and challenges to the selected properties.")
-  }
-
   private lazy val testController = new AppointAgentController(mockRepresentationConnector, StubGroupAccountConnector, mockPropertyLinkConnector, mockAgentsConnector, StubAuthentication)
 
   private lazy val mockPropertyLinkConnector = mock[PropertyLinkConnector]
