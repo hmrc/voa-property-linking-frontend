1a33d81967259d05db2d65878208d7505b292ce6
Jonathan Pike <23456514+jon-pike@users.noreply.github.com>
2019-05-22 14:18:59 +0100

    Vtcca 2656 (#660)
    * [VTCCA-2656][JP] - fixed bug where multiple agents where getting revoked
    
    * [VTCCA-2656][JP] - slight refactor
    
    * [VTCCA-2656][JP] - slight refactor

--- app/controllers/agentAppointment/AppointAgentController.scala
+++ app/controllers/agentAppointment/AppointAgentController.scala
@@ -252,8 +252,9 @@ class AppointAgentController @Inject()(representations: PropertyRepresentationCo
               _ <- Future.traverse(action.propertyLinkIds)(pLink =>
                 createAndSubitAgentRevokeRequest(
                   pLink,
-                  request.organisationId)).recover {
-                case e => Logger.info(s"Failed to get a property link during multiple property agent appointment: ${e.getMessage}")
+                  request.organisationId,
+                  action.agentCode)).recover {
+                case e => Logger.info(s"Failed to get a property link during revoke multiple property agent: ${e.getMessage}")
               }
             } yield
               Ok(views.html.propertyRepresentation.revokeAgentSummary(action, group.companyName))
@@ -307,26 +308,21 @@ class AppointAgentController @Inject()(representations: PropertyRepresentationCo
 
 
   private def createAndSubitAgentRevokeRequest(pLink: String,
-                                               organisationId: Long)(implicit hc: HeaderCarrier): Future[Unit] = {
-    propertyLinks.get(organisationId, pLink.toLong) map {
-      case Some(prop) => {
-        revokeAgents(prop)
-      }
-      case None => {
-        Future.successful(Unit)
-      }
-    }
+                                               organisationId: Long,
+                                               agentCode: Long)(implicit hc: HeaderCarrier): Future[Unit] = {
 
-  }
-
-  private def revokeAgents(link: PropertyLink)(implicit hc: HeaderCarrier): Future[Unit] = {
-    for {
-      revokedAgents <- Future.traverse(link.agents)(agent => representations.revoke(agent.authorisedPartyId))
-    } yield {
-      revokedAgents
+    propertyLinks.get(organisationId, pLink.toLong) flatMap {
+      case Some(link) => link.agents.find(a => a.agentCode == agentCode) match {
+        case Some(agent) => representations.revoke(agent.authorisedPartyId)
+        // shouldn't be possible for agent not to exist in property links
+        // just ignore if it does happen
+        case None => Future.successful(Unit)
+      }
+      // shouldn't be possible for user to select a bad property link
+      // just ignore if it does happen
+      case None => Future.successful(Unit)
     }
   }
-
 
   private def updateAllAgentsPermission(authorisationId: Long, link: PropertyLink, newAgentPermission: AppointAgent,
                                         newAgentOrgId: Long, individualId: Long, organisationId: Long)(implicit hc: HeaderCarrier): Future[Unit] = {
