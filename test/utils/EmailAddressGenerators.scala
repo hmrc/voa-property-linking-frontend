1a8496385a6fec559bd3d70ec9532c45d14fcf2a
Reddy Yattpu <reddy.yattpu@digital.hmrc.gov.uk>
2019-06-19 15:20:55 +0100

    VTCCA-2687 Add email validation on email address when amended in Your details

---
+++ test/utils/EmailAddressGenerators.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import org.scalacheck.Gen._
+import org.scalacheck.{Gen, Shrink}
+
+trait EmailAddressGenerators {
+  def noShrink[T] = Shrink[T](_ => Stream.empty)
+  implicit val dontShrinkStrings: Shrink[String] = noShrink[String]
+
+  def nonEmptyString(char: Gen[Char]) =
+    nonEmptyListOf(char)
+      .map(_.mkString)
+      .suchThat(!_.isEmpty)
+
+  def chars(chars: String) = Gen.choose(0, chars.length - 1).map(chars.charAt)
+
+  val validMailbox = nonEmptyString(oneOf(alphaChar, chars(".!#$%&’'*+/=?^_`{|}~-"))).label("mailbox")
+
+  val validDomain = (for {
+    topLevelDomain <- nonEmptyString(alphaChar)
+    otherParts <- listOf(nonEmptyString(alphaChar))
+  } yield (otherParts :+ topLevelDomain).mkString(".")).label("domain")
+
+  def validEmailAddresses(mailbox: Gen[String] = validMailbox, domain: Gen[String] = validDomain) =
+    for {
+      mailbox <- mailbox
+      domain <- domain
+    } yield s"$mailbox@$domain"
+}
---
+++ test/utils/EmailAddressValidationSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{Matchers, WordSpec}
+
+class EmailAddressValidationSpec extends WordSpec with Matchers with PropertyChecks {
+
+  "Testing EmailAddress" should {
+
+    "invalid email should return false" in {
+      EmailAddressValidation.isValid("sausages") shouldBe false
+      EmailAddressValidation.isValid("sausages@sausages") shouldBe false
+      EmailAddressValidation.isValid("sausages@sausages.") shouldBe false
+      EmailAddressValidation.isValid("@sausages.") shouldBe false
+      EmailAddressValidation.isValid("@sausages.com") shouldBe false
+      EmailAddressValidation.isValid("sausages.com") shouldBe false
+      EmailAddressValidation.isValid("sausages.com") shouldBe false
+    }
+
+    "valid email should return false" in {
+      EmailAddressValidation.isValid("sausages@gmail.com") shouldBe true
+    }
+  }
+}
