8b5eedbe7c78678feaa5e11fd1d396fc5d095671
Reddy Yattapu <tripindicator@gmail.com>
2019-06-28 12:38:26 +0100

    VTCCA-2691 Break line fixed (#673)

--- app/assets/javascripts/src/addressLookup.js
+++ app/assets/javascripts/src/addressLookup.js
@@ -101,7 +101,7 @@
                                 $('#text-form-group input:eq(0)').val(data[index]['addressUnitId']).attr('placeholder', '');
 
                                 if(data[index]['buildingNumber'] != undefined) {
-                                    $('#textAddressData').append("<span>" + data[index]['buildingNumber'] + "</span>");
+                                    $('#textAddressData').append("<span>" + data[index]['buildingNumber'] + "</span><br>");
                                 }
                                 if(data[index]['buildingName'] != undefined) {
                                     $('#textAddressData').append("<span>" +  " "+ data[index]['buildingName']+ "</span><br>");
