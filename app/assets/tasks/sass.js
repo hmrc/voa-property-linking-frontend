6306e121ca10720dbf889ced75aa0ec63c905b8e
Reddy Yattpu <reddy.yattpu@digital.hmrc.gov.uk>
2019-08-28 15:56:12 +0100

    VTCCA-2731 missing sass.js file added

---
+++ app/assets/tasks/sass.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Dependencies
+var gulp = require('gulp'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+	sass = require('gulp-sass');
+
+// Sass task
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            //sourceComments: 'normal'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('../../public/stylesheets'));
+});
+
+// Minify govuk_template css
+gulp.task('minify-css:govuk_template', function () {
+    gulp.src('./node_modules/govuk_template_mustache/assets/stylesheets/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('../../public/stylesheets'));
+});
+
+// Run sass task
+gulp.task('build-sass', [ 'sass','minify-css:govuk_template']);
