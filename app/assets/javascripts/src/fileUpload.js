68989217879a43a6f0dbad7deeeeed6931d4cda0
Habeeb <h.alliowe@gmail.com>
2022-03-21 09:39:39 +0000

    VTCCA-4574 prevent csv files from being uploaded

--- app/assets/javascripts/src/fileUpload.js
+++ app/assets/javascripts/src/fileUpload.js
@@ -68,6 +68,10 @@
                             mime = "image/png";
                             break;
 
+                            case "csv":
+                            mime = "Unknown/Extension missing";
+                            break;
+
                             default:
                             mime = file.type ? file.type : 'Unknown/Extension missing';
                             break;
