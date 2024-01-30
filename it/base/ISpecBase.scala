735d3e4577f34e56a4ef119863881471ac85dcfa
Will Blakeney <60264460+WillBlakeney@users.noreply.github.com>
2024-01-30 13:40:58 +0000

    VTCCA-6203 Interface tests for 'Relationship to property' page (#1270)
    * VTCCA-6203 Interface tests for 'Relationship to property' page
    
    * VTCCA-6203 Update tests to use new test data

--- it/base/ISpecBase.scala
+++ it/base/ISpecBase.scala
@@ -42,6 +42,8 @@ trait ISpecBase extends AnyWordSpec with Matchers with GuiceOneServerPerSuite wi
     "microservice.services.business-rates-authorisation.port" -> mockPort.toString,
     "microservice.services.auth.host" -> mockHost,
     "microservice.services.auth.port" -> mockPort.toString,
+    "microservice.services.vmv.host" -> mockHost,
+    "microservice.services.vmv.port" -> mockPort.toString,
     "business-rates-dashboard-frontend.url" -> "/business-rates-dashboard"
   ) ++ extraConfig
 
