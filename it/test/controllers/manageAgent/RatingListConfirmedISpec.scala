0be9ebb37ee2d50b02753f2afb7f75846d664482
Joe Jones <33491932+joenjones07@users.noreply.github.com>
2025-03-19 14:26:21 +0000

    VTCCA-7469: Added tests to RatingListConfirmedISpec

--- it/test/controllers/manageAgent/RatingListConfirmedISpec.scala
+++ it/test/controllers/manageAgent/RatingListConfirmedISpec.scala
@@ -42,8 +42,11 @@ class RatingListConfirmedISpec extends ISpecBase with HtmlComponentHelpers {
   val headerText = "The rating lists that Test Agent can act for you on has been changed"
   def thisAgentTextSingle(listYear: String) =
     s"This agent can act for you on your property valuations on the $listYear rating list, for properties that you assign to them or they add to your account"
-  val thisAgentTextMultiple =
+
+  val thisAgentTextTwo =
     "This agent can act for you on your property valuations on the 2023 and 2017 rating lists, for properties that you assign to them or they add to your account"
+  val thisAgentTextThree =
+    "This agent can act for you on your property valuations on the 2026, 2023, and 2017 rating lists, for properties that you assign to them or they add to your account"
   val whatHappensText = "What happens next"
   val youCanText = "You can change the rating lists this agent can act for you on at any time."
   val goToYourText = "Go to your account home"
@@ -52,9 +55,11 @@ class RatingListConfirmedISpec extends ISpecBase with HtmlComponentHelpers {
     "Mae’r rhestrau ardrethu y gall Test Agent weithredu arnynt ar eich rhan wedi newid - Valuation Office Agency - GOV.UK"
   val headerTextWelsh = "Mae’r rhestrau ardrethu y gall Test Agent weithredu arnynt ar eich rhan wedi newid"
   def thisAgentTextSingleWelsh(listYear: String) =
-    s"Gall yr asiant hwn weithredu ar eich rhan ar brisiadau eich eiddo o restr ardrethu $listYear, ar gyfer eiddo rydych yn eu neilltuo iddo, ac ar gyfer eiddo y mae’n eu hychwanegu at eich cyfrif"
-  val thisAgentTextMultipleWelsh =
-    "Gall yr asiant hwn weithredu ar eich rhan ar brisiadau eich eiddo o restrau ardrethu 2023 a 2017, ar gyfer eiddo rydych yn eu neilltuo iddo, ac ar gyfer eiddo y mae’n eu hychwanegu at eich cyfrif"
+    s"Gall yr asiant hwn weithredu ar eich rhan ar eich prisiadau eiddo ar restr ardrethu $listYear, ar gyfer eiddo rydych yn eu neilltuo iddo, ac ar gyfer eiddo y mae’n eu hychwanegu at eich cyfrif"
+  val thisAgentTextTwoWelsh =
+    "Gall yr asiant hwn weithredu ar eich rhan ar eich prisiadau eiddo ar restrau ardrethu 2023 a 2017, ar gyfer eiddo rydych yn eu neilltuo iddo, ac ar gyfer eiddo y mae’n eu hychwanegu at eich cyfrif"
+  val thisAgentTextThreeWelsh =
+    "Gall yr asiant hwn weithredu ar eich rhan ar eich prisiadau eiddo ar restrau ardrethu 2026, 2023 a 2017, ar gyfer eiddo rydych yn eu neilltuo iddo, ac ar gyfer eiddo y mae’n eu hychwanegu at eich cyfrif"
   val whatHappensTextWelsh = "Yr hyn sy’n digwydd nesaf"
   val youCanTextWelsh =
     "Gallwch newid pa restrau ardrethu y gall yr asiant hwn weithredu arnynt ar eich rhan ar unrhyw adeg."
@@ -69,7 +74,37 @@ class RatingListConfirmedISpec extends ISpecBase with HtmlComponentHelpers {
   val goHomeHref = "/business-rates-dashboard/home"
 
   "RatingListConfirmedController" should {
-    "Show an English confirmed screen with the correct text when the user has chosen both list years and the language is set to English" which {
+    "Show an English confirmed screen with the correct text when the user has chosen three list years and the language is set to English" which {
+
+      lazy val document = getRatingsListConfirmedPage(English, List("2026", "2023", "2017"))
+
+      s"has a title of $titleText" in {
+        document.title() shouldBe s"$titleText"
+      }
+
+      s"has a panel containing a header of '$headerText'" in {
+        document.select(headerSelector).text shouldBe headerText
+      }
+
+      s"has text on the screen of '$thisAgentTextThree'" in {
+        document.select(thisAgentSelector).text() shouldBe thisAgentTextThree
+      }
+
+      s"has a small heading on the screen of '$whatHappensText'" in {
+        document.select(whatHappensSelector).text() shouldBe whatHappensText
+      }
+
+      s"has text on the screen of '$youCanText'" in {
+        document.select(youCanSelector).text() shouldBe youCanText
+      }
+
+      s"has a link on the screen of '$goToYourText'" in {
+        document.select(goToYourSelector).text() shouldBe goToYourText
+        document.select(goToYourSelector).attr("href") shouldBe goHomeHref
+      }
+    }
+
+    "Show an English confirmed screen with the correct text when the user has chosen two list years and the language is set to English" which {
 
       lazy val document = getRatingsListConfirmedPage(English, List("2023", "2017"))
 
@@ -81,8 +116,8 @@ class RatingListConfirmedISpec extends ISpecBase with HtmlComponentHelpers {
         document.select(headerSelector).text shouldBe headerText
       }
 
-      s"has text on the screen of '$thisAgentTextMultiple'" in {
-        document.select(thisAgentSelector).text() shouldBe thisAgentTextMultiple
+      s"has text on the screen of '$thisAgentTextTwo'" in {
+        document.select(thisAgentSelector).text() shouldBe thisAgentTextTwo
       }
 
       s"has a small heading on the screen of '$whatHappensText'" in {
@@ -129,7 +164,37 @@ class RatingListConfirmedISpec extends ISpecBase with HtmlComponentHelpers {
       }
     }
 
-    "Show a Welsh confirmed screen with the correct text when the user has chosen both list years and the language is set to Welsh" which {
+    "Show a Welsh confirmed screen with the correct text when the user has chosen three list years and the language is set to Welsh" which {
+
+      lazy val document = getRatingsListConfirmedPage(Welsh, List("2026", "2023", "2017"))
+
+      s"has a title of $titleText in welsh" in {
+        document.title() shouldBe s"$titleTextWelsh"
+      }
+
+      s"has a panel containing a header of '$headerText' in welsh" in {
+        document.select(headerSelector).text shouldBe headerTextWelsh
+      }
+
+      s"has text on the screen of '$thisAgentTextThree' in welsh" in {
+        document.select(thisAgentSelector).text() shouldBe thisAgentTextThreeWelsh
+      }
+
+      s"has a small heading on the screen of '$whatHappensText' in welsh" in {
+        document.select(whatHappensSelector).text() shouldBe whatHappensTextWelsh
+      }
+
+      s"has text on the screen of '$youCanText' in welsh" in {
+        document.select(youCanSelector).text() shouldBe youCanTextWelsh
+      }
+
+      s"has a link on the screen of '$goToYourText' in welsh" in {
+        document.select(goToYourSelector).text() shouldBe goToYourTextWelsh
+        document.select(goToYourSelector).attr("href") shouldBe goHomeHref
+      }
+    }
+
+    "Show a Welsh confirmed screen with the correct text when the user has chosen two list years and the language is set to Welsh" which {
 
       lazy val document = getRatingsListConfirmedPage(Welsh, List("2023", "2017"))
 
@@ -141,8 +206,8 @@ class RatingListConfirmedISpec extends ISpecBase with HtmlComponentHelpers {
         document.select(headerSelector).text shouldBe headerTextWelsh
       }
 
-      s"has text on the screen of '$thisAgentTextMultiple' in welsh" in {
-        document.select(thisAgentSelector).text() shouldBe thisAgentTextMultipleWelsh
+      s"has text on the screen of '$thisAgentTextTwo' in welsh" in {
+        document.select(thisAgentSelector).text() shouldBe thisAgentTextTwoWelsh
       }
 
       s"has a small heading on the screen of '$whatHappensText' in welsh" in {
