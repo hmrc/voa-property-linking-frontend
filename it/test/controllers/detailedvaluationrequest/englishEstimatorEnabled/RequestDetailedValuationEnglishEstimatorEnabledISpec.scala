47482e36f53f3cf95dee84da39159458118d2566
Joe Jones <33491932+joenjones07@users.noreply.github.com>
2026-01-19 13:25:44 +0000

    VTCCA-8157: ITests for englishEstimatorEnabled/Disabled

--- it/test/controllers/detailedvaluationrequest/englishEstimatorEnabled/RequestDetailedValuationEnglishEstimatorEnabledISpec.scala
+++ it/test/controllers/detailedvaluationrequest/englishEstimatorEnabled/RequestDetailedValuationEnglishEstimatorEnabledISpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package controllers.detailedvaluationrequest
+package controllers.detailedvaluationrequest.englishEstimatorEnabled
 
 import base.{HtmlComponentHelpers, ISpecBase}
 import com.github.tomakehurst.wiremock.client.WireMock._
@@ -29,10 +29,10 @@ import uk.gov.hmrc.http.{HeaderCarrier, SessionId}
 
 import java.util.UUID
 
-class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers {
+class RequestDetailedValuationEnglishEstimatorEnabledISpec extends ISpecBase with HtmlComponentHelpers {
 
   override lazy val extraConfig: Map[String, String] =
-    Map("feature-switch.draftListEnabled" -> "true", "feature-switch.englishEstimatorEnabled" -> "false", "feature-switch.welshEstimatorEnabled" -> "true")
+    Map("feature-switch.draftListEnabled" -> "true", "feature-switch.englishEstimatorEnabled" -> "true", "feature-switch.welshEstimatorEnabled" -> "true")
 
   override def submissionId = "PL1ZRPBP7"
   override def uarn: Long = 7651789000L
@@ -84,26 +84,16 @@ class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers 
   val rateableValueSelector = "#future-valuation-rv"
 
   val valuationTabInsetText =
-    "The estimator tool for England has been removed ahead of business rates bills being issued by local councils. The Valuation Office Agency can help with any questions about your rateable value. For more information on how your bill will be calculated click here (opens in new tab). If you have any questions about your bill you should contact your local council."
+    "This will be the rateable value for the property. It is not what you will pay in business rates or rent. Your local council uses the rateable value to calculate the business rates bill."
   val valuationTabInsetTextWelsh =
-    "The estimator tool for England has been removed ahead of business rates bills being issued by local councils. The Valuation Office Agency can help with any questions about your rateable value. For more information on how your bill will be calculated click here (opens in new tab). If you have any questions about your bill you should contact your local council."
+    "Dyma fydd y gwerth ardrethol ar gyfer yr eiddo. Nid dyma’r swm byddwch yn ei dalu mewn ardrethi busnes neu rent. Mae eich cyngor lleol yn defnyddio’r gwerth ardrethol er mwyn cyfrifo’r bil ardrethi busnes."
   val valuationTabInsetTextSelector = "#future-valuation-inset-rv > p:nth-child(1)"
 
-  val valuationTabInsetWelshPropertyText =
-    "This will be the rateable value for the property. It is not what you will pay in business rates or rent. Your local council uses the rateable value to calculate the business rates bill."
-  val valuationTabInsetWelshPropertyTextWelsh =
-    "Dyma fydd y gwerth ardrethol ar gyfer yr eiddo. Nid dyma’r swm byddwch yn ei dalu mewn ardrethi busnes neu rent. Mae eich cyngor lleol yn defnyddio’r gwerth ardrethol er mwyn cyfrifo’r bil ardrethi busnes."
-
-  val valuationTabInsetLinkText = "here (opens in new tab)"
-  val valuationTabInsetLinkTextWelsh = "here (opens in new tab)"
-  val valuationTabInsetLinkTextSelector = "#future-estimate-link"
+  val valuationTabInsetLinkText = "Estimate what the business rates bill may be from 1 April 2026"
+  val valuationTabInsetLinkTextWelsh = "Amcangyfrif beth all y bil ardrethi busnes fod o 1 Ebrill 2026"
   val valuationTabInsetLinkHref =
-    "https://www.gov.uk/calculate-your-business-rates"
-
-  val valuationTabInsetLinkWelshPropertyText = "Estimate what the business rates bill may be from 1 April 2026"
-  val valuationTabInsetLinkWelshPropertyTextWelsh = "Amcangyfrif beth all y bil ardrethi busnes fod o 1 Ebrill 2026"
-  val valuationTabInsetLinkWelshPropertyHref =
     "http://localhost:9300/business-rates-find/estimate-your-business-rates/start-from-dvr-valuation?authorisationId=1&propertyLinkSubmissionId=PL1ZRPBP7&valuationId=10028428&isOwner=true&uarn=7651789000&tabName=valuation-tab"
+  val valuationTabInsetLinkTextSelector = "#future-estimate-link"
 
   val valuationTabP1 =
     "This detailed valuation will not be available until 1 April 2026. You will be able to request it from that date."
@@ -472,284 +462,6 @@ class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers 
     }
   }
 
-  "myOrganisationAlreadyRequestedDetailValuation displays the correct content for viewing a draft valuation - English (English Property pub)" which {
-    lazy val document: Document = getPage(English, false, true)
-
-    s"has a title of $titleText" in {
-      document.title() shouldBe titleText
-    }
-
-    s"has a caption of $captionText" in {
-      document.select(captionTextSelector).text shouldBe captionText
-    }
-
-    s"has a heading of $h1Text" in {
-      document.select(h1TextSelector).text shouldBe h1Text
-    }
-
-    s"has text and value for $councilRefText" in {
-      document.select(councilRefTextSelector).text shouldBe councilRefText
-      document.select(councilRefValueSelector).text shouldBe "AMBER-VALLEY-REF"
-    }
-
-    s"has a $valuationTabText tab" in {
-      document.select(valuationTabTextSelector).text shouldBe valuationTabText
-    }
-
-    s"has a tab heading of $valuationTabHeading" in {
-      document.select(valuationTabHeadingSelector).text shouldBe valuationTabHeading
-    }
-
-    s"has a tab caption of $valuationTabCaption" in {
-      document.select(valuationTabCaptionSelector).text shouldBe valuationTabCaption
-    }
-
-    s"has a paragraph of $valuationP1" in {
-      document.select(valuationP1Selector).text shouldBe valuationP1
-    }
-
-    s"has a rateable value of $rateableValue" in {
-      document.select(rateableValueSelector).text shouldBe rateableValue
-    }
-
-    "has correct inset text" in {
-      document.select(valuationTabInsetTextSelector).text shouldBe valuationTabInsetText
-    }
-
-    s"has correct link text of $valuationTabInsetLinkText" in {
-      document.select(valuationTabInsetLinkTextSelector).text shouldBe valuationTabInsetLinkText
-      document.select(valuationTabInsetLinkTextSelector).attr("href") shouldBe "Should be a download link here"
-    }
-
-    "has correct p1 content" in {
-      document.select(valuationTabP1Selector).text shouldBe valuationTabP1
-    }
-
-    "has correct p2 content" in {
-      document.select(valuationTabP2Selector).text shouldBe valuationTabP2
-    }
-
-    s"has a $valuationHelpTabText tab" in {
-      document.select(valuationHelpTabTextSelector).text shouldBe valuationHelpTabText
-    }
-
-    s"has a tab heading of $valuationHelpTabHeadingText" in {
-      document.select(valuationHelpTabHeadingTextSelector).text shouldBe valuationHelpTabHeadingText
-    }
-
-    s"has a section heading of $valuationHelpTabRvMayChangeHeadingText" in {
-      document
-        .select(valuationHelpTabRvMayChangeHeadingTextSelector)
-        .text shouldBe valuationHelpTabRvMayChangeHeadingText
-    }
-
-    s"has correct content within $valuationHelpTabRvMayChangeHeadingText section" in {
-      document.select(valuationHelpTabRvMayChangeTextSelector).text shouldBe valuationHelpTabRvMayChangeText
-    }
-
-    s"has a section heading of $valuationHelpTabPropertyDetailsChangeHeadingText" in {
-      document
-        .select(valuationHelpTabPropertyDetailsChangeHeadingTextSelector)
-        .text shouldBe valuationHelpTabPropertyDetailsChangeHeadingText
-    }
-
-    s"has correct content within $valuationHelpTabPropertyDetailsChangeHeadingText section" in {
-      document
-        .select(valuationHelpTabPropertyDetailsChangeTextSelector)
-        .text shouldBe valuationHelpTabPropertyDetailsChangeText
-    }
-
-    s"has a section heading of $valuationHelpTabRvTooHighHeadingText" in {
-      document.select(valuationHelpTabRvTooHighHeadingTextSelector).text shouldBe valuationHelpTabRvTooHighHeadingText
-    }
-
-    s"has correct p1 content within $valuationHelpTabPropertyDetailsChangeHeadingText section" in {
-      document.select(valuationHelpTabRvTooHighP1TextSelector).text shouldBe valuationHelpTabRvTooHighP1Text
-    }
-
-    s"has correct p2 content within $valuationHelpTabPropertyDetailsChangeHeadingText section" in {
-      document.select(valuationHelpTabRvTooHighP2TextSelector).text shouldBe valuationHelpTabRvTooHighP2Text
-    }
-
-    s"has a section heading of $valuationHelpTabValuationQuestionsHeadingText" in {
-      document
-        .select(valuationHelpTabValuationQuestionsHeadingTextSelector)
-        .text shouldBe valuationHelpTabValuationQuestionsHeadingText
-    }
-
-    s"has correct p1 content within $valuationHelpTabValuationQuestionsHeadingText section" in {
-      document
-        .select(valuationHelpTabValuationQuestionsTextSelector)
-        .text shouldBe valuationHelpTabValuationQuestionsText
-    }
-
-    s"has correct link content within $valuationHelpTabValuationQuestionsHeadingText section" in {
-      document
-        .select(valuationHelpTabValuationQuestionsLinkTextSelector)
-        .text shouldBe valuationHelpTabValuationQuestionsLinkText
-    }
-
-    s"has link for $link1Text" in {
-      document.select(link1Selector).text shouldBe link1Text
-      document.select(link1Selector).attr("href") shouldBe link1Href
-    }
-
-    s"has link for $link2Text" in {
-      document.select(link2Selector).text shouldBe link2Text
-      document.select(link2Selector).attr("href") shouldBe link2Href
-    }
-
-    s"has link for $link3Text" in {
-      document.select(link3Selector).text shouldBe link3Text
-      document.select(link3Selector).attr("href") shouldBe link3Href
-    }
-
-    s"has link for $link4Text" in {
-      document.select(link4Selector).text shouldBe link4Text
-      document.select(link4Selector).attr("href") shouldBe link4Href
-    }
-  }
-
-  "myOrganisationAlreadyRequestedDetailValuation displays the correct content for viewing a draft valuation - Welsh (English Property pub)" which {
-    lazy val document: Document = getPage(Welsh, false, true)
-
-    s"has a title of $titleText" in {
-      document.title() shouldBe titleTextWelsh
-    }
-
-    s"has a caption of $captionText" in {
-      document.select(captionTextSelector).text shouldBe captionTextWelsh
-    }
-
-    s"has a heading of $h1Text" in {
-      document.select(h1TextSelector).text shouldBe h1TextWelsh
-    }
-
-    s"has text and value for $councilRefText" in {
-      document.select(councilRefTextSelector).text shouldBe councilRefTextWelsh
-      document.select(councilRefValueSelector).text shouldBe "AMBER-VALLEY-REF"
-    }
-
-    s"has a $valuationTabText tab" in {
-      document.select(valuationTabTextSelector).text shouldBe valuationTabTextWelsh
-    }
-
-    s"has a tab heading of $valuationTabHeading" in {
-      document.select(valuationTabHeadingSelector).text shouldBe valuationTabHeadingWelsh
-    }
-
-    s"has a tab caption of $valuationTabCaption" in {
-      document.select(valuationTabCaptionSelector).text shouldBe valuationTabCaptionWelsh
-    }
-
-    s"has a paragraph of $valuationP1" in {
-      document.select(valuationP1Selector).text shouldBe valuationP1TextWelsh
-    }
-
-    s"has a rateable value of $rateableValue" in {
-      document.select(rateableValueSelector).text shouldBe rateableValue
-    }
-
-    "has correct inset text" in {
-      document.select(valuationTabInsetTextSelector).text shouldBe valuationTabInsetTextWelsh
-    }
-
-    s"has correct link text of $valuationTabInsetLinkText" in {
-      document.select(valuationTabInsetLinkTextSelector).text shouldBe valuationTabInsetLinkTextWelsh
-      document.select(valuationTabInsetLinkTextSelector).attr("href") shouldBe "Should be a download link here"
-    }
-
-    "has correct p1 content" in {
-      document.select(valuationTabP1Selector).text shouldBe valuationTabP1Welsh
-    }
-
-    "has correct p2 content" in {
-      document.select(valuationTabP2Selector).text shouldBe valuationTabP2Welsh
-    }
-
-    s"has a $valuationHelpTabText tab" in {
-      document.select(valuationHelpTabTextSelector).text shouldBe valuationHelpTabTextWelsh
-    }
-
-    s"has a tab heading of $valuationHelpTabHeadingText" in {
-      document.select(valuationHelpTabHeadingTextSelector).text shouldBe valuationHelpTabHeadingTextWelsh
-    }
-
-    s"has a section heading of $valuationHelpTabRvMayChangeHeadingText" in {
-      document
-        .select(valuationHelpTabRvMayChangeHeadingTextSelector)
-        .text shouldBe valuationHelpTabRvMayChangeHeadingTextWelsh
-    }
-
-    s"has correct content within $valuationHelpTabRvMayChangeHeadingText section" in {
-      document.select(valuationHelpTabRvMayChangeTextSelector).text shouldBe valuationHelpTabRvMayChangeTextWelsh
-    }
-
-    s"has a section heading of $valuationHelpTabPropertyDetailsChangeHeadingText" in {
-      document
-        .select(valuationHelpTabPropertyDetailsChangeHeadingTextSelector)
-        .text shouldBe valuationHelpTabPropertyDetailsChangeHeadingTextWelsh
-    }
-
-    s"has correct content within $valuationHelpTabPropertyDetailsChangeHeadingText section" in {
-      document
-        .select(valuationHelpTabPropertyDetailsChangeTextSelector)
-        .text shouldBe valuationHelpTabPropertyDetailsChangeTextWelsh
-    }
-
-    s"has a section heading of $valuationHelpTabRvTooHighHeadingText" in {
-      document
-        .select(valuationHelpTabRvTooHighHeadingTextSelector)
-        .text shouldBe valuationHelpTabRvTooHighHeadingTextWelsh
-    }
-
-    s"has correct p1 content within $valuationHelpTabPropertyDetailsChangeHeadingText section" in {
-      document.select(valuationHelpTabRvTooHighP1TextSelector).text shouldBe valuationHelpTabRvTooHighP1TextWelsh
-    }
-
-    s"has correct p2 content within $valuationHelpTabPropertyDetailsChangeHeadingText section" in {
-      document.select(valuationHelpTabRvTooHighP2TextSelector).text shouldBe valuationHelpTabRvTooHighP2TextWelsh
-    }
-
-    s"has a section heading of $valuationHelpTabValuationQuestionsHeadingText" in {
-      document
-        .select(valuationHelpTabValuationQuestionsHeadingTextSelector)
-        .text shouldBe valuationHelpTabValuationQuestionsHeadingTextWelsh
-    }
-
-    s"has correct p1 content within $valuationHelpTabValuationQuestionsHeadingText section" in {
-      document
-        .select(valuationHelpTabValuationQuestionsTextSelector)
-        .text shouldBe valuationHelpTabValuationQuestionsTextWelsh
-    }
-
-    s"has correct link content within $valuationHelpTabValuationQuestionsHeadingText section" in {
-      document
-        .select(valuationHelpTabValuationQuestionsLinkTextSelector)
-        .text shouldBe valuationHelpTabValuationQuestionsLinkTextWelsh
-    }
-
-    s"has link for $link1Text" in {
-      document.select(link1Selector).text shouldBe link1TextWelsh
-      document.select(link1Selector).attr("href") shouldBe link1Href
-    }
-
-    s"has link for $link2Text" in {
-      document.select(link2Selector).text shouldBe link2TextWelsh
-      document.select(link2Selector).attr("href") shouldBe link2Href
-    }
-
-    s"has link for $link3Text" in {
-      document.select(link3Selector).text shouldBe link3TextWelsh
-      document.select(link3Selector).attr("href") shouldBe link3Href
-    }
-
-    s"has link for $link4Text" in {
-      document.select(link4Selector).text shouldBe link4TextWelsh
-      document.select(link4Selector).attr("href") shouldBe link4Href
-    }
-  }
-
   "myOrganisationAlreadyRequestedDetailValuation displays the correct content for viewing a draft valuation - English (Welsh Property)" which {
     lazy val document: Document = getPage(English, true)
 
@@ -788,12 +500,12 @@ class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers 
     }
 
     "has correct inset text" in {
-      document.select(valuationTabInsetTextSelector).text shouldBe valuationTabInsetWelshPropertyText
+      document.select(valuationTabInsetTextSelector).text shouldBe valuationTabInsetText
     }
 
-    s"has correct link text of $valuationTabInsetLinkWelshPropertyText" in {
-      document.select(valuationTabInsetLinkTextSelector).text shouldBe valuationTabInsetLinkWelshPropertyText
-      document.select(valuationTabInsetLinkTextSelector).attr("href") shouldBe valuationTabInsetLinkWelshPropertyHref
+    s"has correct link text of $valuationTabInsetLinkText" in {
+      document.select(valuationTabInsetLinkTextSelector).text shouldBe valuationTabInsetLinkText
+      document.select(valuationTabInsetLinkTextSelector).attr("href") shouldBe valuationTabInsetLinkHref
     }
 
     "has correct p1 content" in {
@@ -922,12 +634,12 @@ class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers 
     }
 
     "has correct inset text" in {
-      document.select(valuationTabInsetTextSelector).text shouldBe valuationTabInsetWelshPropertyTextWelsh
+      document.select(valuationTabInsetTextSelector).text shouldBe valuationTabInsetTextWelsh
     }
 
     s"has correct link text of $valuationTabInsetLinkText" in {
-      document.select(valuationTabInsetLinkTextSelector).text shouldBe valuationTabInsetLinkWelshPropertyTextWelsh
-      document.select(valuationTabInsetLinkTextSelector).attr("href") shouldBe valuationTabInsetLinkWelshPropertyHref
+      document.select(valuationTabInsetLinkTextSelector).text shouldBe valuationTabInsetLinkTextWelsh
+      document.select(valuationTabInsetLinkTextSelector).attr("href") shouldBe valuationTabInsetLinkHref
     }
 
     "has correct p1 content" in {
@@ -1039,7 +751,7 @@ class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers 
 
   private def getPage(language: Language, welshProperty: Boolean = false, allowedEstimatorScatCode: Boolean = false): Document = {
 
-    getRequestStubs(welshProperty, allowedEstimatorScatCode)
+    getRequestStubs(welshProperty ,allowedEstimatorScatCode)
 
     val res = await(
       ws.url(
@@ -1060,7 +772,7 @@ class RequestDetailedValuationISpec extends ISpecBase with HtmlComponentHelpers 
 
   def getRequestStubs(welshProperty: Boolean = false, allowedEstimatorScatCode: Boolean = false): StubMapping = {
 
-    val stubbedPropertyHistory = if(allowedEstimatorScatCode) testPropertyHistory.copy(history = Seq(testPropertyValuation.copy(scatCode = Some("062")))) else testPropertyHistory
+    val stubbedPropertyHistory = if(allowedEstimatorScatCode) testPropertyHistory.copy(history = Seq(testPropertyValuation.copy(scatCode = Some("303")))) else testPropertyHistory
     val stubApiAssessment = testApiAssessments(assessments = List(if (welshProperty) draftApiAssessmentWelsh else draftApiAssessment))
 
     authStubs
